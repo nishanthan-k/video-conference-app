0933605231e8593384b222f9f12a4f552791ff05
Nishanthan K <89657905+nishanthan-k@users.noreply.github.com>
2024-05-24 21:25:29 +0530

    render video

--- src/components/RenderVideo.jsx
+++ src/components/RenderVideo.jsx
@@ -1,28 +1,71 @@
-import { useEffect } from "react";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const RenderVideo = () => {
-  const user1 = useRef()
-  const user2 = useRef()
+  const [localStream, setLocalStram] = useState("");
+  const [remoteStream, setRemoteStram] = useState("");
+  const user1 = useRef();
+  const user2 = useRef();
 
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
-        user1.current.srcObject = stream;
-      } catch (error) {
-        console.log("Error on connecting audio / video:", error)
-      }
+  const init = async (ref) => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: false,
+      });
+      ref.current.srcObject = stream;
+    } catch (error) {
+      console.log("Error on connecting audio / video:", error);
     }
+  };
 
-    init()
-  }, [])
+  init(user1);
+
+  const servers = {
+    iceServers: [
+      {
+        urls: [
+          "stun:stun1.1.google.com:19302",
+          "stun:stun2.1.google.com:19302",
+        ],
+      },
+    ],
+  };
+
+  const createOffer = async () => {
+    const peerConnection = new RTCPeerConnection(servers);
+
+    const stream = new MediaStream();
+    user2.current.srcObject = stream;
+
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription();
+
+    console.log(offer);
+  };
 
   return (
     <div>
       <div className="w-full pt-10 flex justify-around">
-        <video ref={user1} autoPlay playsInline className="w-2/5 h-72 bg-slate-300"></video>
-        <video ref={user2} autoPlay playsInline className="w-2/5 h-72 bg-slate-300"></video>
+        <div className="w-1/3 py-10 bg-slate-400 flex flex-col items-center gap-5">
+          <video
+            ref={user1}
+            autoPlay
+            playsInline
+            className="w-full h-72"
+          ></video>
+          <button className="bg-sky-500 px-4 py-2" onClick={createOffer}>
+            Create Offer
+          </button>
+        </div>
+        <div className="w-2/5 py-10 bg-slate-400 flex flex-col items-center gap-5">
+          <video
+            ref={user2}
+            autoPlay
+            playsInline
+            className="w-full h-72"
+          ></video>
+          <button className="bg-sky-500 px-4 py-2">Create Offer</button>
+        </div>
       </div>
     </div>
   );
